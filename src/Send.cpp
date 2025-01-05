87a78d8a328d8d6b8aa3dbe4a4efd20ffeb8b3ff
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-01-05 22:55:35 +0600

    sender done

--- src/Send.cpp
+++ src/Send.cpp
@@ -1,92 +1,58 @@
 /*
- * SimpleSender.cpp
- *
- *  Demonstrates sending IR codes in standard format with address and command
- *  An extended example for sending can be found as SendDemo.
- *
- *  Copyright (C) 2020-2022  Armin Joachimsmeyer
- *  armin.joachimsmeyer@gmail.com
- *
- *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
- *
- *  MIT License
- */
+    * If you want to send a raw HEX value directly like e.g. 0xCB340102 you must use sendNECRaw()
+    * example
+    * Serial.println(F("Send 32 bit LSB 0xCB340102 with NECRaw()"));
+    * IrSender.sendNECRaw(0xCB340102, sRepeats);
+*/ 
+
+/*
+    * If you want to send an "old" MSB HEX value used by IRremote versions before 3.0 like e.g. 0x40802CD3 you must use sendNECMSB()
+    * example
+    * Serial.println(F("Send old 32 bit MSB 0x40802CD3 with sendNECMSB()"));
+    * IrSender.sendNECMSB(0x40802CD3, 32, sRepeats);
+*/
+
+/*
+    * Set up the data to be sent.
+    * For most protocols, the data is build up with a constant 8 (or 16 byte) address
+    * and a variable 8 bit command.
+    * There are exceptions like Sony and Denon, which have 5 bit address.
+*/
+
+    // Protocol=PulseDistance Raw-Data=0x55AA2AD5 97 bits LSB first
+    // Send on a 32 bit platform with: 
+    // uint64_t tRawData[]={0x56A900FF00FF00FF, 0x55AA2AD5};
+    // IrSender.sendPulseDistanceWidthFromArray(38, 6100, 7300, 600, 1650, 600, 550, &tRawData[0], 97, PROTOCOL_IS_LSB_FIRST, <RepeatPeriodMillis>, <numberOfRepeats>);
+
+    // Protocol=NEC Address=0x0 Command=0x40 Raw-Data=0xBF40FF00 32 bits LSB first
+    // Send with: IrSender.sendNEC(0x0, 0x40, <numberOfRepeats>);
+
 #include <Arduino.h>
+#include <IRremote.hpp> // include the library
 
 //#define SEND_PWM_BY_TIMER         // Disable carrier PWM generation in software and use (restricted) hardware PWM.
 //#define USE_NO_SEND_PWM           // Use no carrier PWM, just simulate an active low receiver signal. Overrides SEND_PWM_BY_TIMER definition
 
-/*
- * This include defines the actual pin number for pins like IR_RECEIVE_PIN, IR_SEND_PIN for many different boards and architectures
- */
-const uint16_t IR_RECEIVE_PIN = 14; // D5 on a NodeMCU board.
+const uint16_t IR_SEND_PIN = 4; // D5 on a NodeMCU board.
 
-#include <IRremote.hpp> // include the library
+uint8_t sRepeats = 0;
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
 
     Serial.begin(115200);
 
-    // Just to know which program is running on my Arduino
-    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-    Serial.print(F("Send IR signals at pin "));
-    Serial.println(IR_SEND_PIN);
-
-    /*
-     * The IR library setup. That's all!
-     */
-    IrSender.begin(); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
-    disableLEDFeedback(); // Disable feedback LED at default feedback LED pin
+    IrSender.begin(IR_SEND_PIN); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
 }
 
-/*
- * Set up the data to be sent.
- * For most protocols, the data is build up with a constant 8 (or 16 byte) address
- * and a variable 8 bit command.
- * There are exceptions like Sony and Denon, which have 5 bit address.
- */
-uint8_t sCommand = 0x34;
-uint8_t sRepeats = 0;
-
 void loop() {
-    /*
-     * Print current send values
-     */
-    Serial.println();
-    Serial.print(F("Send now: address=0x00, command=0x"));
-    Serial.print(sCommand, HEX);
-    Serial.print(F(", repeats="));
-    Serial.print(sRepeats);
-    Serial.println();
-
     Serial.println(F("Send standard NEC with 8 bit address"));
     Serial.flush();
 
     // Receiver output for the first loop must be: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
-    IrSender.sendNEC(0x00, sCommand, sRepeats);
+    IrSender.sendNEC(0x0, 0x40, <numberOfRepeats>);     //Light control, can also be sent using raw data 0xBF40FF00
 
-    /*
-     * If you want to send a raw HEX value directly like e.g. 0xCB340102 you must use sendNECRaw()
-     */
-//    Serial.println(F("Send 32 bit LSB 0xCB340102 with NECRaw()"));
-//    IrSender.sendNECRaw(0xCB340102, sRepeats);
-
-    /*
-     * If you want to send an "old" MSB HEX value used by IRremote versions before 3.0 like e.g. 0x40802CD3 you must use sendNECMSB()
-     */
-//    Serial.println(F("Send old 32 bit MSB 0x40802CD3 with sendNECMSB()"));
-//    IrSender.sendNECMSB(0x40802CD3, 32, sRepeats);
-
-    /*
-     * Increment send values
-     */
-    sCommand += 0x11;
-    sRepeats++;
-    // clip repeats at 4
-    if (sRepeats > 4) {
-        sRepeats = 4;
-    }
+    uint64_t tRawData[]={0x56A900FF00FF00FF, 0x55AA2AD5};
+    IrSender.sendPulseDistanceWidthFromArray(38, 6100, 7300, 600, 1650, 600, 550, &tRawData[0], 97, PROTOCOL_IS_LSB_FIRST,0,0);
 
     delay(1000);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 }
